6277fb4053325e2b3f7ea299ffaef02b0020cae0
Konstantin <konstantin@example.com>
2025-04-18 13:51:52 +0900

    Updated database name and configuration

--- models.py
+++ models.py
@@ -36,6 +36,7 @@ class User(Base):
 
 # Подключение к PostgreSQL
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL:", DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
